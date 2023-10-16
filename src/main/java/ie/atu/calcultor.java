681e3739d5613ba3fa3ee62af0e0b87977b62df1
Tyrese <g00421432@atu.ie>
2023-10-16 10:40:54 +0100

    created subtraction in calculator

--- src/main/java/ie/atu/calcultor.java
+++ src/main/java/ie/atu/calcultor.java
@@ -9,6 +9,8 @@ public class calcultor {
         add();
         System.out.println("Multiplication");
         delete();
+        System.out.println("Subtraction");
+        delete2();
 
     }
 
@@ -39,4 +41,16 @@ public class calcultor {
         System.out.println("The total is " + total);
     }
 
+    public static void delete2()
+    {
+        System.out.println("Please enter your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter you second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber - secondNumber;
+        System.out.println("The total is " + total);
+    }
 }
