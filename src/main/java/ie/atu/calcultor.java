46bd6cc22ae6831d67f0e72eace30f58ef7ee399
Tyrese <g00421432@atu.ie>
2023-10-16 10:37:43 +0100

    created multiplication in calculator

--- src/main/java/ie/atu/calcultor.java
+++ src/main/java/ie/atu/calcultor.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class calcultor {
 
     public static void main(String[] args) {
+        System.out.println("Addition");
         add();
+        System.out.println("Multiplication");
+        delete();
 
     }
 
@@ -22,4 +25,18 @@ public class calcultor {
         int total = firstNumber + secondNumber;
         System.out.println("The total is " + total);
     }
+
+    public static void delete()
+    {
+        System.out.println("Please enter your first number :");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter you second number :");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The total is " + total);
+    }
+
 }
