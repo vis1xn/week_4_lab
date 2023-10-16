3d93ff4bb7e5511758b91686007b7b51c7bf1e4c
Tyrese <g00421432@atu.ie>
2023-10-16 09:58:49 +0100

    Created new line of code

--- src/main/java/ie/atu/calcultor.java
+++ src/main/java/ie/atu/calcultor.java
@@ -1,7 +1,7 @@
 package ie.atu;
 
-public class Main {
+public class calcultor {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello");
     }
 }
